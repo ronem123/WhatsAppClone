bc20adb5c62543108eccf43e6ba686703b0a732e
Ram Mandal <ram.cp18113@gmail.com>
2019-07-09 23:56:23 +0545

    whats app home modified

--- lib/whatsapp_home.dart
+++ lib/whatsapp_home.dart
@@ -3,10 +3,10 @@ import 'package:whats_app_clone/pages/tab_call_screen.dart';
 import 'package:whats_app_clone/pages/tab_camera_screen.dart';
 import 'package:whats_app_clone/pages/tab_chat_screen.dart';
 import 'package:whats_app_clone/pages/tab_status_screen.dart';
+import 'package:camera/camera.dart';
 
 class WhatsAppHome extends StatefulWidget {
-  var cameras;
-  WhatsAppHome(this.cameras);
+  List<CameraDescription> cameras;
 
   @override
   _WhatsAppHomeState createState() => new _WhatsAppHomeState();
@@ -20,7 +20,12 @@ class _WhatsAppHomeState extends State<WhatsAppHome>
   void initState() {
     // TODO: implement initState
     super.initState();
+    setUpCamera();
     _tabController = new TabController(vsync: this, initialIndex: 1, length: 4);
+  }
+
+  Future<void> setUpCamera() async {
+    widget.cameras = await availableCameras();
   }
 
   @override
